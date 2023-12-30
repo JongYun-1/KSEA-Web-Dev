057caaefcc3e94d9a4a5c4f6e6cd1ecc7de48047
John Won <johnwon@Johns-Macbook-Pro.local>
2023-12-30 13:05:03 +0900

    Refactored with functions and updated member's page

--- src/components/membersPage/membersPage.js
+++ src/components/membersPage/membersPage.js
@@ -12,51 +12,54 @@ import team_picture from '../../assets/IMG_2629.jpg'
 import test_picture from '../../assets/5138BA98-19A9-4C8B-BE4C-8AB0B4AD8A57.png'
 import logo from '../../assets/KSEA YG PURDUE LOGO.png'
 
-function MembersPage(props) {
+/* Member introduction profile functions */
+function PersonCard(props) {
+  const { name, role, major, email } = props;
+
   return (
-    <div>
-      <div id="heading">
-                
-                <div id='heading_text'>
-                    <a href = "/">
-                    <img alt = "heading_text" width= "300vw" height='auto'  src={logo}/>
-                    {/* <img alt = "heading_text" width="500" height="150" src={logo}/> */}
+      <div className = "col-lg-4">
+          <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
+                  dy=".3em">140x140</text>
+          </svg>
+          <h4>{name} <span className="text-muted"> | {role}</span></h4>
+          <p>{major}</p>
+          <p>{email}</p>
+      </div>
+  );
+}
 
-                    </a>
+function Header() {
+  return (
+    <div id = "heading">
+      <div id='heading_text'>
+        <a href = "/">
+          <img alt = "heading_text" width= "300vw" height='auto'  src={logo}/>
+          {/* <img alt = "heading_text" width="500" height="150" src={logo}/> */}
+        </a>
 
-                    <Nav>
-                        {/* <NavDropdown id='nav_text' title='About'>
-                        <NavDropdown.Item id='nav_subtext' href="/about">About KSEA</NavDropdown.Item>
-                        <NavDropdown.Item id='nav_subtext' href="/members">Members</NavDropdown.Item>
-                        <NavDropdown.Item id='nav_subtext' href="/events">Events</NavDropdown.Item>
-                        </NavDropdown> */}
-                        {/* <Nav.Link id='nav_text' href="/about">About KSEA</Nav.Link>
-                        <Nav.Link id='nav_text' href="/members">Members</Nav.Link>
-                        <Nav.Link id='nav_text' href="/events">Events</Nav.Link>
+        <Nav>
+            <NavDropdown id='nav_text' title='About'>
+            <NavDropdown.Item id='nav_subtext' href="/about">About KSEA</NavDropdown.Item>
+            <NavDropdown.Item id='nav_subtext' href="/members">Members</NavDropdown.Item>
+            <NavDropdown.Item id='nav_subtext' href="/events">Events</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link id='nav_text' href="/calendar">Calendar</Nav.Link>
+            <Nav.Link id='nav_text' href="/announcement">Announcement</Nav.Link>
+            <Nav.Link id='nav_text'>Photo</Nav.Link>
+            <Nav.Link id='nav_text' href="/research">Research</Nav.Link>
+        </Nav>
+      </div>
+    </div>
+  )
+}
 
-                        <Nav.Link id='nav_text' href="/calendar">Calendar</Nav.Link>
-                        <Nav.Link id='nav_text' href="/announcement">Announcement</Nav.Link>
-                        <Nav.Link id='nav_text'>Photo</Nav.Link>
-                        <Nav.Link id='nav_text' href="/research">Research</Nav.Link> */}
-
-                        <NavDropdown id='nav_text' title='About'>
-                        <NavDropdown.Item id='nav_subtext' href="/about">About KSEA</NavDropdown.Item>
-                        <NavDropdown.Item id='nav_subtext' href="/members">Members</NavDropdown.Item>
-                        <NavDropdown.Item id='nav_subtext' href="/events">Events</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link id='nav_text' href="/calendar">Calendar</Nav.Link>
-                        <Nav.Link id='nav_text' href="/announcement">Announcement</Nav.Link>
-                        <Nav.Link id='nav_text'>Photo</Nav.Link>
-                        <Nav.Link id='nav_text' href="/research">Research</Nav.Link>
-                    </Nav>
-                </div>
-               
-
-            </div>
+function Members() {
+  return (
       <div id='body'>
         <hr className="body_line" align="center"></hr>
-
-
         <div className='body_jumbo'>
           <Jumbotron id='jumbo_body'>
             <h1 className='jumbo_title'>Who are we?</h1>
@@ -64,79 +67,13 @@ function MembersPage(props) {
               KSEA YG PURDUE는 퍼듀의 한인 이공계 학생들의 발전을 위해 힘쓰는 동아리입니다.
               </p>
             <div className="row">
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>김수민 <span className="text-muted"> | President</span></h4>
-                  <p>Electrical Engineering</p>
-                  <p>kim2419@purdue.edu</p>
-                </div>
-
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>조정연 <span className="text-muted"> | Vice President</span></h4>
-                  <p>Chemical Engineering</p>
-                  <p>cho409@purdue.edu</p>
-                </div>
-
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>정윤경 <span className="text-muted"> | Treasurer</span></h4>
-                  <p>Chemical Engineering</p>
-                  <p>jeong111@purdue.edu</p>
-                </div>
-
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>변지현 <span className="text-muted"> | Secretary</span></h4>
-                  <p>Computer Science</p>
-                  <p>pyunj@purdue.edu</p>
-                </div>
-
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>조성완 <span className="text-muted"> | Event Manager</span></h4>
-                  <p>Material Science Engineering</p>
-                  <p>jo46@purdue.edu</p>
-                </div>
-
-                <div className="col-lg-4">
-                  <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#ebe8e6" /><text x="50%" y="50%" fill="#82918b"
-                      dy=".3em">140x140</text>
-                  </svg>
-                  <h4>이지원 <span className="text-muted"> | Ad Manager</span></h4>
-                  <p>UX Design</p>
-                  <p>lee3400@purdue.edu</p>
-                </div>
-
-              </div>
+              <PersonCard name="Nawon Lee" role="President" major="Genetics" email="placeholder@purdue.edu" />
+              <PersonCard name="Jinwoo Jeong" role="Vice-President" major="Computer Science" email="placeholder@purdue.edu" />
+              <PersonCard name="Seungbin Kwon" role="Treasurer" major="Biomedical Engineering" email="placeholder@purdue.edu" />
+              <PersonCard name="Seungju Park" role="Public Relations TL" major="Industrial Engineering" email="placeholder@purdue.edu" />
+              <PersonCard name="공 석" role="Event TL" major="Placeholder" email="placeholder@purdue.edu" />
+              <PersonCard name="Jongyun Won" role="Website TL" major="Computer Science" email="placeholder@purdue.edu" />
+            </div>
           </Jumbotron>
         </div>
         <hr className="body_line" align="center"></hr>
@@ -149,7 +86,14 @@ function MembersPage(props) {
                 In 2020 by web dev team
             </footer>
       </div>
+  )
+}
 
+function MembersPage(props) {
+  return (
+    <div>
+    <Header />
+    <Members />
     </div>
   )
 }
